626ab4699941c31a7ca1d8a445a316ecf175182e
shihan00321 <shihan001@naver.com>
2025-06-06 22:36:15 +0900

    [Refactor] : 쿼리 실행을 Sql 객체에서 하도록 리팩토링

--- src/main/java/com/back/simpleDb/SimpleDb.java
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -2,12 +2,10 @@ package com.back.simpleDb;
 
 import lombok.Setter;
 
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Setter
 public class SimpleDb {
@@ -24,89 +22,26 @@ public class SimpleDb {
     }
 
     public int run(String query, Object... params) {
-        return execute(pstmt -> pstmt.executeUpdate(), query, params);
-    }
-
-    public Map<String, Object> select(String query) {
-        return execute(pstmt -> {
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return resultSetToMap(rs);
-                }
-                return null;
-            }
-        }, query);
-    }
-
-    public List<Map<String, Object>> selectAll(String query) {
-        return execute(pstmt -> {
-            try (ResultSet rs = pstmt.executeQuery()) {
-                List<Map<String, Object>> result = new ArrayList<>();
-                while (rs.next()) {
-                    result.add(resultSetToMap(rs));
-                }
-                return result;
-            }
-        }, query);
-    }
-
-    public LocalDateTime selectDatetime(String query) {
-        return execute(pstmt -> {
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                if (resultSet.next()) {
-                    return resultSet.getTimestamp(resultSet.getRow()).toLocalDateTime();
-                }
-                return null;
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }, query);
-    }
-
-    public Long selectLong(String query) {
-        return execute(pstmt -> {
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                return resultSet.next() ? resultSet.getLong(1) : null;
-            }
-        }, query);
-    }
-
-    public String selectString(String query) {
-        return execute(pstmt -> {
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                return resultSet.next() ? resultSet.getString(1) : null;
-            }
-        }, query);
-    }
-
-    private Map<String, Object> resultSetToMap(ResultSet rs) throws SQLException {
-        Map<String, Object> row = new LinkedHashMap<>();
-        ResultSetMetaData metaData = rs.getMetaData();
-        int columnCount = metaData.getColumnCount();
-        for (int i = 1; i <= columnCount; i++) {
-            String columnName = metaData.getColumnName(i);
-            row.put(columnName, rs.getObject(i));
-        }
-        return row;
-    }
-
-    public Sql genSql() {
-        return new Sql(this);
-    }
-
-    private <T> T execute(QueryExecutor<T> queryExecutor, String query, Object... params) {
         try (Connection connection = getConnection();
              PreparedStatement pstmt = connection.prepareStatement(query)) {
             for (int i = 0; i < params.length; i++) {
                 pstmt.setObject(i + 1, params[i]);
             }
-            return queryExecutor.execute(pstmt);
+            return pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, username, password);
+    public Sql genSql() {
+        return new Sql(getConnection());
+    }
+
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/back/simpleDb/Sql.java
+++ src/main/java/com/back/simpleDb/Sql.java
@@ -1,55 +1,111 @@
 package com.back.simpleDb;
 
+import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Sql {
     private final StringBuilder query = new StringBuilder();
-    private final List<Object> parameterList = new ArrayList<>();
-    private final SimpleDb simpleDb;
+    private final List<Object> params = new ArrayList<>();
+    private final Connection connection;
 
-    public Sql(SimpleDb simpleDb) {
-        this.simpleDb = simpleDb;
+    public Sql(Connection connection) {
+        this.connection = connection;
     }
 
     public Sql append(String part, Object... params) {
         query.append(part).append("\n");
-        parameterList.addAll(Arrays.asList(params));
+        this.params.addAll(Arrays.asList(params));
         return this;
     }
 
-    public long insert() {
-        return simpleDb.run(query.toString(), parameterList.toArray());
+    public int insert() {
+        return executeUpdate();
     }
 
     public int update() {
-        return simpleDb.run(query.toString(), parameterList.toArray());
+        return executeUpdate();
     }
 
     public int delete() {
-        return simpleDb.run(query.toString(), parameterList.toArray());
-    }
-
-    public List<Map<String, Object>> selectRows() {
-        return simpleDb.selectAll(query.toString());
-    }
-
-    public Map<String, Object> selectRow() {
-        return simpleDb.select(query.toString());
+        return executeUpdate();
     }
 
     public LocalDateTime selectDatetime() {
-        return simpleDb.selectDatetime(query.toString());
+        return execute(pstmt -> {
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getTimestamp(resultSet.getRow()).toLocalDateTime();
+                }
+                return null;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public Long selectLong() {
-        return simpleDb.selectLong(query.toString());
+        return execute(pstmt -> {
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                return resultSet.next() ? resultSet.getLong(1) : null;
+            }
+        });
     }
 
     public String selectString() {
-        return simpleDb.selectString(query.toString());
+        return execute(pstmt -> {
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                return resultSet.next() ? resultSet.getString(1) : null;
+            }
+        });
+    }
+
+    public Map<String, Object> selectRow() {
+        return execute(pstmt -> {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return resultSetToMap(rs);
+                }
+                return null;
+            }
+        });
+    }
+
+    public List<Map<String, Object>> selectRows() {
+        return execute(pstmt -> {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<Map<String, Object>> result = new ArrayList<>();
+                while (rs.next()) {
+                    result.add(resultSetToMap(rs));
+                }
+                return result;
+            }
+        });
+    }
+
+    private Map<String, Object> resultSetToMap(ResultSet rs) throws SQLException {
+        Map<String, Object> row = new LinkedHashMap<>();
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        for (int i = 1; i <= columnCount; i++) {
+            String columnName = metaData.getColumnName(i);
+            row.put(columnName, rs.getObject(i));
+        }
+        return row;
+    }
+
+    private int executeUpdate() {
+        return execute(PreparedStatement::executeUpdate);
+    }
+
+    private <T> T execute(QueryExecutor<T> queryExecutor) {
+        try (PreparedStatement pstmt = connection.prepareStatement(query.toString())) {
+            for (int i = 0; i < params.size(); i++) {
+                pstmt.setObject(i + 1, params.get(i));
+            }
+            return queryExecutor.execute(pstmt);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
